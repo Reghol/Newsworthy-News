f4f9027991e46169e6c13b2f7821ca2a592deada
Reghol <jakubnc@protonmail.com>
2019-11-01 09:42:15 +0000

    configuring heroku

--- connection.js
+++ connection.js
@@ -1,1 +1,9 @@
-module.exports = require('knex')(require('./knexfile'));
+const ENV = process.env.NODE_ENV || 'development';
+const knex = require('knex');
+
+const dbConfig =
+  ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('./knexfile');
+
+module.exports = knex(dbConfig);
